1c3a1b234531838bb0c6c927c200df1b6a5170c0
deepfates <deepfates@gmail.com>
2022-06-24 14:17:12 -0600

    mock data for local dev

--- views/page1.py
+++ views/page1.py
@@ -3,36 +3,37 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import html, dcc
+from dash import html, dcc, Input, Output, State
 from zero import ZeroClient
-
+from app import app
 from config import config
 
 # This is mock data representing small amounts of MOB sent out over the last 90 days
-# data = pd.read_csv("mob_sent_out.csv")
+data = pd.read_json("test_data.json")
 
 # Make a zero client and use it to call the ledger API
 client = ZeroClient("0.0.0.0", 5559)
 # records = False
 # if not records:
-records = client.call("call_ledger_method", "get_all_txs")
-data = pd.DataFrame(records)
-data["amount_usd"] = data["amount_usd_cents"] / 100
-data["amount_mob"] = data["amount_pmob"] / 1e12
-logging.info(data)
+# records = client.call("call_ledger_method", "get_all_txs")
+# data = pd.DataFrame(records)
+# data["amount_usd"] = data["amount_usd_cents"] / 100
+# data["amount_mob"] = data["amount_pmob"] / 1e12
+# data["account_short"] = [x[:7] for x in data["account"]]
+# logging.info(data)
+
+data.to_json("test_data.json")
 
 # This is a histogram of accumulation per user
 user_payouts = go.Figure()
 user_payouts.add_trace(
-    go.Histogram(x=data["account"], y=data["amount_usd"], histfunc="sum")
+    go.Histogram(x=data["account_short"], y=data["amount_usd"], histfunc="sum")
 )
 user_payouts.add_trace(
-    go.Histogram(x=data["account"], y=data["amount_mob"], histfunc="sum")
+    go.Histogram(x=data["account_short"], y=data["amount_mob"], histfunc="sum")
 )
 user_payouts.update_layout(barmode="overlay")
 user_payouts.update_traces(opacity=0.7)
-
-# Get a the mob_
 
 # This is a line graph of accumulated MOB sent out over time
 payouts_over_time = px.line(data, x="ts", y=data["amount_usd"].cumsum())
@@ -44,7 +45,7 @@ emergency_button = [
         [
             html.H5("Emergency Stop", className="card-title"),
             html.P(
-                "Pressing this button will immediately stop all MOB distribution. This cannot be undone.",
+                "Pressing this button will immediately stop all MOB distribution. ",
                 className="card-text",
             ),
             dbc.Row(
@@ -56,14 +57,26 @@ emergency_button = [
                             color="danger",
                             id="emergency-stop",
                             size="lg",
+                            n_clicks=0,
                         )
                     ),
                     dbc.Col(),
                 ]
             ),
+            html.Div(id="stop-output", children="Not yet"),
         ]
     ),
 ]
+
+
+@app.callback(
+    Output("stop-output", "children"),
+    Input("emergency-stop", "n_clicks"),
+)
+def stop_payments(n_clicks):
+    client.call("stop_payments", "auth")
+    if n_clicks > 1:
+        return "Payments have been stopped."
 
 
 ###############################################################################
